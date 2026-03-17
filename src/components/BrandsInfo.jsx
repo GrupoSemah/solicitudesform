07a0568c84f8a13a12cb85ee9f700b1a09acb36b
Roberto J. Vargas <rvargas@rv-solutions.net>
2026-03-17 12:09:20 -0500

    fix: send proper sucursal names instead of abbreviations

--- src/components/BrandsInfo.jsx
+++ src/components/BrandsInfo.jsx
@@ -2,47 +2,47 @@ export const BrandsInfo = ({ register, setValue }) => {
   const handleSelectChange = (event) => {
     const selectedSucursal = event.target.value
     const sucursalEmails = {
-      vh: [
-        "vistahermosa@almacenajes.net",
-        "ventas.vistahermosa@almacenajes.net",
-        "callcenter2@almacenajes.net",
-        "callcenter3@almacenajes.net",
-      ],
-      ce: [
-        "costadeleste@almacenajes.net",
-        "ventas.costadeleste@almacenajes.net",
-        "callcenter2@almacenajes.net",
-        "callcenter3@almacenajes.net",
-      ],
-      ra: [
-        "rioabajo@almacenajes.net",
-        "ventas.rioabajo@almacenajes.net",
-        "callcenter2@almacenajes.net",
-        "callcenter3@almacenajes.net",
-      ],
-      al: [
-        "albrook@almacenajes.net",
-        "ventas.albrook@almacenajes.net",
-        "callcenter2@almacenajes.net",
-        "callcenter3@almacenajes.net",
-      ],
-      sa: [
-        "sanantonio@almacenajes.net",
-        "ventas.sanantonio@almacenajes.net",
-        "callcenter2@almacenajes.net",
-        "callcenter3@almacenajes.net",
-      ],
-      co: ["colon@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
-      mocho: [
+      "Milla 8": [
         "milla8@almacenajes.net",
         "ventas.milla8@almacenajes.net",
         "callcenter2@almacenajes.net",
         "callcenter3@almacenajes.net",
       ],
-      go: ["gorgona@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
-      da: ["david@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
-      hm: ["hatomontana@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
-      tm: ["ventas.tumbamuerto@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
+      "Vista Hermosa": [
+        "vistahermosa@almacenajes.net",
+        "ventas.vistahermosa@almacenajes.net",
+        "callcenter2@almacenajes.net",
+        "callcenter3@almacenajes.net",
+      ],
+      "Costa del Este": [
+        "costadeleste@almacenajes.net",
+        "ventas.costadeleste@almacenajes.net",
+        "callcenter2@almacenajes.net",
+        "callcenter3@almacenajes.net",
+      ],
+      "Rio Abajo": [
+        "rioabajo@almacenajes.net",
+        "ventas.rioabajo@almacenajes.net",
+        "callcenter2@almacenajes.net",
+        "callcenter3@almacenajes.net",
+      ],
+      "Albrook": [
+        "albrook@almacenajes.net",
+        "ventas.albrook@almacenajes.net",
+        "callcenter2@almacenajes.net",
+        "callcenter3@almacenajes.net",
+      ],
+      "San Antonio": [
+        "sanantonio@almacenajes.net",
+        "ventas.sanantonio@almacenajes.net",
+        "callcenter2@almacenajes.net",
+        "callcenter3@almacenajes.net",
+      ],
+      "Colón": ["colon@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
+      "Gorgona": ["gorgona@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
+      "David": ["david@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
+      "Hato Montaña": ["hatomontana@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
+      "Tumba Muerto": ["ventas.tumbamuerto@almacenajes.net", "callcenter2@almacenajes.net", "callcenter3@almacenajes.net"],
     }
     const emails = sucursalEmails[selectedSucursal] || []
     const emailsText = emails.join(", ")
@@ -63,17 +63,17 @@ export const BrandsInfo = ({ register, setValue }) => {
           className="border border-gray-300 px-4 py-2 rounded-md w-full"
         >
           <option value="">Seleccione:</option>
-          <option value="vh">Vista Hermosa</option>
-          <option value="ce">Costa del Este</option>
-          <option value="ra">Rio Abajo</option>
-          <option value="al">Albrook</option>
-          <option value="sa">San Antonio</option>
-          <option value="co">Colón</option>
-          <option value="mocho">Milla 8</option>
-          <option value="go">Gorgona</option>
-          <option value="da">David</option>
-          <option value="hm">Hato Montaña</option>
-          <option value="tm">Tumba Muerto</option>
+          <option value="Milla 8">Milla 8</option>
+          <option value="Vista Hermosa">Vista Hermosa</option>
+          <option value="Costa del Este">Costa del Este</option>
+          <option value="Rio Abajo">Rio Abajo</option>
+          <option value="Albrook">Albrook</option>
+          <option value="San Antonio">San Antonio</option>
+          <option value="Colón">Colón</option>
+          <option value="Gorgona">Gorgona</option>
+          <option value="David">David</option>
+          <option value="Hato Montaña">Hato Montaña</option>
+          <option value="Tumba Muerto">Tumba Muerto</option>
         </select>
       </div>
 
